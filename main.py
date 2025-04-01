5b8f60ae2340f83b2be5513ba3f338ecd620cb57
TheSlumberingEgg <aramawad05@gmail.com>
2025-04-01 16:08:41 -0400

    fix screen display

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
 
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT():
+            if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (125, 0, 255))
         pygame.display.flip()
